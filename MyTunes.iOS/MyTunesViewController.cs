fdcf143110292857f614cea722bc9818d906cc95
AK33M <akeemtaiwo@outlook.com>
2017-04-09 12:02:56 +0100

    initial commit and shared project module

---
+++ MyTunes.iOS/MyTunesViewController.cs
@@ -0,0 +1,39 @@
+﻿using UIKit;
+using System.Linq;
+
+namespace MyTunes
+{
+	public class MyTunesViewController : UITableViewController
+	{
+		public override void ViewDidLayoutSubviews()
+		{
+			base.ViewDidLayoutSubviews();
+
+			if (UIDevice.CurrentDevice.CheckSystemVersion(7,0)) {
+				TableView.ContentInset = new UIEdgeInsets (20, 0, 0, 0);
+			}
+		}
+
+		public async override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			//TableView.Source = new ViewControllerSource<string>(TableView) {
+			//	DataSource = new string[] { "One", "Two", "Three" },
+			//};
+
+			var songList = await SongLoader.Load();
+
+			var viewControllerSource = new ViewControllerSource<Song>(TableView)
+			{
+				DataSource = songList.ToList(),
+				TextProc = (arg) => arg.Name,
+				DetailTextProc = (arg) => $"{arg.Artist} - {arg.Album}"
+			};
+
+			TableView.Source = viewControllerSource;
+		}
+	}
+
+}
+
